874bb21e78a050ca045d31787603fa1f5c9c87b6
Anelise Estevam <anelisee1@gmail.com>
2025-06-11 23:02:28 +0100

    Add robots.txt and dynamic sitemap.xml for SEO indexing

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const baseUrl = 'https://aneliseestevam.com';
+  const pages = [
+    '',
+    'about',
+    'projects',
+    'contact',
+  ];
+  const urls = pages.map(
+    (page) => `<url><loc>${baseUrl}/${page}</loc></url>`
+  ).join('');
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  return new NextResponse(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+} 
